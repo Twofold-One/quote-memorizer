593f3a142a30d122f0359fd6afd462a1b608c051
Twofold-One <evan_geras@mail.ru>
2022-02-06 14:32:43 +0700

    home, showQuote and createQuote routers created

--- main.go
+++ main.go
@@ -1,13 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-// home is handler function.
+// home is main page handler function.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from quote-memorizer"))
+}
+
+// showQuote is quote show handler function.
+func showQuote(w http.ResponseWriter, r *http.Request) {
+	// get id from request, convert it to int and check if
+	// value less than 1 response 404
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Show chosen quote with ID %d...\n", id)
+}
+
+// createQuote is quote creation handler function.
+func createQuote(w http.ResponseWriter, r *http.Request) {
+
+
+	// r.Method checks if request is POST.
+	if r.Method != http.MethodPost {
+
+		// Header().Set() adds "Allow: POST" to the header
+		w.Header().Set("Allow", http.MethodPost)
+		// http.Error sends status code and a message
+		http.Error(w, "Method is restricted", http.StatusMethodNotAllowed)
+		return 
+	}
+	w.Write([]byte("Form to create new quote"))
 }
 
 func main() {
@@ -15,6 +50,8 @@ func main() {
 	//  and registers home function as handler to URL template "/".
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/quote", showQuote)
+	mux.HandleFunc("/quote/create", createQuote)
 
 	// Server startup
 	log.Println("Server is running on http://127.0.0.1:4000")
